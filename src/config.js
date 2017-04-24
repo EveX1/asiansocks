9dc532585204f131cbab9aac2ffcd36d9540d0fd
EveX1 <evekill@free.fr>
2017-04-24 13:04:37 +0200

    config parser

---
+++ src/config.js
@@ -0,0 +1,70 @@
+import fs from 'fs';
+import ArgParseObj from 'argparse';
+
+const ArgParse = ArgParseObj.ArgumentParser;
+
+const parser = new ArgParse({
+    version: '0.0.1',
+    addHelp: true,
+    description: 'config.yml parser'
+});
+
+parser.addArgument(
+    ['-a', '--api'], {
+        help: 'API a utiliser (twitter)'
+    }
+);
+
+parser.addArgument(
+    ['-ck', '--consumerkey'], {
+        help: 'Twitter Customer Key'
+    }
+);
+
+parser.addArgument(
+    ['-cs', '--consumersecret'], {
+        help: 'Twitter Customer secret key'
+    }
+);
+
+parser.addArgument(
+    ['-t', '--token'], {
+        help: 'Twitter Token'
+    }
+);
+
+parser.addArgument(
+    ['-ts', '--tokensecret'], {
+        help: 'Twitter Token secret'
+    }
+);
+
+parser.addArgument(
+    ['', '--ip'], {
+        help: 'IP de la base de données'
+    }
+);
+
+parser.addArgument(
+    ['-p', '--port'], {
+        help: 'Port de la base de données'
+    }
+);
+
+parser.addArgument(
+    ['', '--db'], {
+        help: 'Nom de la base de données'
+    }
+);
+
+const configSample = "./config/config.sample.yml";
+const config = "./config/config.yml";
+
+if (fs.existsSync(config)) {
+    fs.unlinkSync(config);
+}
+
+fs.readFileSync(configSample, "utf8");
+fs.appendFileSync(config, )
+
+const args = parser.parseArgs();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('test');
+import fs from 'fs';
+import ArgParseObj from 'argparse';
+
+const ArgParse = ArgParseObj.ArgumentParser;
+
+const parser = new ArgParse({
+    version: '0.0.1',
+    addHelp: true,
+    description: 'config.yml parser'
+});
+
+parser.addArgument(
+    ['-k', '--keywords'], {
+        help: 'Liste de mots à utiliser pour recherche flux twitter'
+    }
+);
+
+parser.addArgument(
+    ['-c', '--config'], {
+        help: 'Fichier de configuration pour API Twitter'
+    }
+);
+
+
+const config = "./config/";
+
+const args = parser.parseArgs();
+
+fs.readFileSync(`${config}${args.config}`, "utf8");

e8e681f0fe93fd5866367f3b78623252223f6425
PochWar <ripoche.b@gmail.com>
2017-04-24 12:46:52 +0200

    recuperation des infos du fichier config.yml

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-/**
- * Created by poch on 24/04/2017.
- */
-console.log('test');
+/*
+*PACKAGES
+*/
+//lire les infos d'un fichier yml
+const yaml = require('js-yaml');
+//lire et ecrure dansun fichier
+const fs   = require('fs');
+
+
+
+/*
+* RECUPERATION DE CONFIG.YML
+*/
+let config;
+try {
+    config = yaml.safeLoad(fs.readFileSync('./config/config.yml', 'utf8'));
+} catch (e) {
+    console.log(e);
+}
+console.log(config.default.api.twitter.consumerkey);
+console.log(config.default.api.twitter.consumersecret);
+console.log(config.default.api.twitter.accestoken);
+console.log(config.default.api.twitter.accesstokensecret);
+console.log(config.default.db.ip);
+console.log(config.default.db.port);
+console.log(config.default.db.db);

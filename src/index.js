d216a9f5b17d6c506d04153a596b0a17b090909b
EveX1 <evekill@free.fr>
2017-04-26 15:16:23 +0200

    server up

--- src/index.js
+++ src/index.js
@@ -3,7 +3,45 @@
  */
 import userExtract from './UserExtract';
 import Config from './Config';
+import express from 'express';
+import bodyParser from 'body-parser';
 
+/*
+ * EXPRESS
+ */
+
+const app = express();
+const title = "Asian Socks"
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '/../views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', (req, res) => res.render('index', {
+    title: `Bienvenue sur ${title}`
+}));
+
+app.get('/config', (req, res) => res.render('config', {
+    title: `Configuration de ${title}`
+}));
+
+app.post('/config', (req, res) => {
+    res.render('index', {
+        consumerkey: req.body.consumerkey
+    });
+});
+
+app.listen(3000, () => console.log("Connected on: 3000"));
+
+
+
+
+/*
+ * FONCTIONNEMENT APPLI
+ */
 // récupérer le fichier config
 const configClass = new Config();
 const config = configClass.getConfig();
